136a3a49289b7f2f1e71be478fc438d06184b5b3
Noris4est <48294724+Noris4est@users.noreply.github.com>
2020-04-14 21:39:11 +0300

    New modules developed and testbenches made
    A module combining PWM_FPGA and SPI_SLAVE has been developed. His work is that, depending on the incoming data packet, the duty cycle of the PWM signal is set. For better synchronization, it is recommended to reset the module during the absence of busy SPI_SLAVE, which is on its board, in other words, it is recommended to do a regular reset if CS == 1. Thus, SPI_SLAVE_PWM is a PWM device managed through SPI.
    
    Разработан модуль, совмещающий PWM_FPGA и SPI_SLAVE. Его работа заключаетя в том, что в зависимости от приходящего пакета данных выставляется скважность шим сигнала. Для лучшей синхронизации рекомендуется делать сброс модуля во время отсутствия занятости SPI_SLAVE, который находится на его борту, иными словами, рекомендуется делать регулярно сброс если CS==1. Таким образом, SPI_SLAVE_PWM является шим устройством управляемый через SPI
    
    A testbench was developed for the interaction of SPI_MASTER and SPI_SLAVE_PWM, in which (its results) the necessary information on the operation of the module is displayed.
    
    Разработан тестбенч взамиодействия SPI_MASTER и SPI_SLAVE_PWM, в котором (его результатах) показывается необходимая информация о работе модуля .

---
+++ Testbenches/SPI_SLAVE_PWM.v
@@ -0,0 +1,67 @@
+module SPI_SLAVE_PWM
+#(
+	parameter 	CPHA								=1,	//clock phase
+	parameter	CPOL								=1,	//clock polarity
+	parameter 	PACK_LENGTH 					=8,	//number of bits in package 
+	parameter 	PACK_BIT_SEQUENCE_TRANSMIT	=1,	//1-major bit forward;0-junior bit forward;
+	parameter 	PACK_BIT_SEQUENCE_RECEIVE	=1,	//1-major bit forward;0-junior bit forward;								
+	parameter 	CLOCK_FREQUENCY				=50000000,
+	parameter 	MAX_VALUE						=2**PACK_LENGTH-1,
+	parameter 	PWM_FREQUENCY					=CLOCK_FREQUENCY/16,  //(MAX_VALUE-1)
+	parameter 	DEEP_FILL_FACTOR				=PACK_LENGTH
+)
+(
+	input IN_CLOCK,
+	input MOSI,
+	input CS,
+	input SCLK,
+	input IN_RESET,
+	output OUT_PWM_SIGNAL
+);
+	wire IN_ENABLE;
+	assign IN_ENABLE=1'b1;
+	
+	wire [DEEP_FILL_FACTOR-1:0] IN_PWM_FILL_FACTOR;
+	wire [PACK_LENGTH-1:0] OUT_RECEIVE_DATA;
+	assign IN_PWM_FILL_FACTOR=OUT_RECEIVE_DATA;
+	
+	assign IN_RESET_SLAVE=IN_RESET;
+	assign IN_RESET_PWM=CS;
+	
+	PWM_FPGA
+	#(
+		.CLOCK_FREQUENCY(CLOCK_FREQUENCY),
+		.PWM_FREQUENCY(PWM_FREQUENCY),
+		.MAX_VALUE(MAX_VALUE),
+		.DEEP_FILL_FACTOR(DEEP_FILL_FACTOR)
+	)
+	PWM_MODULE
+	(
+		IN_CLOCK,
+		IN_RESET_PWM,
+		IN_ENABLE,
+		IN_PWM_FILL_FACTOR,
+		OUT_PWM_SIGNAL
+	);
+	
+	SPI_FPGA_SLAVE
+	#(
+		.CPHA(CPHA),
+		.CPOL(CPOL),
+		.PACK_LENGTH(PACK_LENGTH),
+		.PACK_BIT_SEQUENCE_TRANSMIT(PACK_BIT_SEQUENCE_TRANSMIT),
+		.PACK_BIT_SEQUENCE_RECEIVE(PACK_BIT_SEQUENCE_RECEIVE)
+	)
+	SLAVE_MODULE
+	(
+		IN_TRANSMIT_DATA,
+		MOSI,
+		CS,
+		SCLK,
+		IN_RESET_SLAVE,
+		MISO,
+		OUT_RECEIVE_DATA
+	);
+	
+
+endmodule
